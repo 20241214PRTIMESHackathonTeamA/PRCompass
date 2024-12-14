bd19ed025ad354326a296b1ff6212d64e2d9c1e7
いね <iineineno03k@gmail.com>
2024-12-14 16:12:18 +0900

    fix

--- backend/prcompass/src/main/java/com/example/prcompass/controller/PrCompassController.java
+++ backend/prcompass/src/main/java/com/example/prcompass/controller/PrCompassController.java
@@ -1,5 +1,6 @@
 package com.example.prcompass.controller;
 
+import com.example.prcompass.controller.response.JudgeResult;
 import com.example.prcompass.controller.response.RecommendResponse;
 import com.example.prcompass.controller.response.SimilarResponse;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import java.util.Map;
 public class PrCompassController {
     @GetMapping("/title-recommend")
     public ResponseEntity<List<RecommendResponse>> getTitleRecommendations() {
+        //TODO notionのやつ呼び出し。
         var res1 = new RecommendResponse("1", "株式会社ほげ", "2024年12月14日", "Title 1", 10);
         var res2 = new RecommendResponse("2", "株式会社ふが", "2024年12月14日", "Title 2", 20);
 
@@ -22,14 +24,16 @@ public class PrCompassController {
     }
 
     @PostMapping("/judge")
-    public ResponseEntity<Map<String, String>> judgeTitle(@RequestBody String title) {
-        String result = (title != null && !title.isEmpty()) ? "Accepted" : "Rejected";
+    public ResponseEntity<JudgeResult> judgeTitle(@RequestBody String title) {
+        //TODO: 3観点の結果を返す。
+        var result = new JudgeResult(false, false, false);
 
-        return ResponseEntity.ok(Map.of("result", result));
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping("/similar")
     public ResponseEntity<List<SimilarResponse>> findSimilarTitles(@RequestBody String title) {
+        //TODO: スクレイピングのやつ呼び出し。
         var res1 = new SimilarResponse("1", "株式会社ほげ", "2024年12月14日", "Title 1", 10);
         var res2 = new SimilarResponse("2", "株式会社ふが", "2024年12月14日", "Title 2", 20);
 
