9356f63e2b45ac24a9581b34c2c8697dfb9e8352
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-14 18:36:30 +0900

    [ADD] Logo component

---
+++ client/src/components/Logo/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'Logo',
+})
+
+</script>
+
+<template>
+  <img src="@/assets/PRCompassLogo.png"
+    alt="PRCompassLogo"
+    width="225px"
+    height="208px"
+    class="logo"
+  />
+</template>
+<style lang="css" scoped>
+.logo {
+  display: block;
+}
+</style>
