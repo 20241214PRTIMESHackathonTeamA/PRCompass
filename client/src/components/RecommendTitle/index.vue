c8a575d29dc3e4419d7afdb5c94b7ae19591ddc5
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-15 00:56:22 +0900

    [ADD] Recommend Title component

---
+++ client/src/components/RecommendTitle/index.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+defineOptions({
+  name: 'RecommendTitle',
+})
+
+interface Props {
+  topicName?: string
+}
+
+interface Emits {
+  (event: 'submit-recommend-button'): void
+}
+
+const emits = defineEmits<Emits>()
+const props = defineProps<Props>()
+
+const handleButtonSubmit = () => {
+  emits('submit-recommend-button')
+}
+
+
+</script>
+
+<template>
+  <div class="recommend-container">
+    <div class="recommend-name">
+      {{ props?.topicName }}
+    </div>
+    <button
+      @click="handleButtonSubmit()"
+      type="button"
+      class="arrow-button"
+    >
+      <img src="@/assets/icon_nav_right_arrow_.png" />
+    </button>
+  </div>
+
+</template>
+
+<style lang="css" scoped>
+.recommend-container {
+  width: 682px;
+  height: 73px;
+  font-size: 13px;
+
+  display: inline-flex;
+  align-items: center;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  overflow: hidden;
+  background-color: #fff;
+}
+.recommend-container .recommend-name {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  height: 100%;
+  border: none;
+  outline: none;
+  font-size: 16px;
+  flex: 1;
+  color: var(--color-black);
+  padding-left: 13px;
+}
+.arrow-button {
+  width: 45px;
+  height: 100%;
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.arrow-button:hover {
+  background-color: var(--color-light-gray);
+}
+
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,6 +4,7 @@ import Logo from '@/components/Logo/index.vue'
 import Introduction from '@/components/Introduction/index.vue'
 import OrLine from '@/components/OrLine/index.vue'
 import InputTitle from '@/components/InputTitle/index.vue'
+import RecommendTitle from '@/components/RecommendTitle/index.vue'
 
 const handleInputTitleSubmit = (value: string) => {
   //TODO: request post server
@@ -34,8 +35,6 @@ fetchRecommendations()
     <InputTitle class="input-title" @submit-input-arrow="handleInputTitleSubmit" />
     <OrLine class="or-line" />
 
-    <h1>Recommended Titles</h1>
-
     <!-- ローディング中の表示 -->
     <div v-if="titleRecommendStore.loading">Loading...</div>
 
@@ -43,11 +42,15 @@ fetchRecommendations()
     <div v-else-if="titleRecommendStore.error">Error: {{ titleRecommendStore.error }}</div>
 
     <!-- 推奨タイトルのリスト -->
-    <ul v-else>
-      <li v-for="(topic, index) in titleRecommendStore.recommendations" :key="index">
-        {{ topic }}
-      </li>
-    </ul>
+    <div v-else>
+      <div v-for="(topic, index) in titleRecommendStore.recommendations" :key="index">
+        <RecommendTitle
+          class="recommend-title"
+          :topic-name="topic"
+          @submit-recommend-button=""
+        />
+      </div>
+    </div>
 
     <!-- 再取得ボタン -->
     <button @click="fetchRecommendations">Reload Recommendations</button>
@@ -75,5 +78,9 @@ fetchRecommendations()
 .or-line {
   margin-bottom: 66px;
 }
+.recommend-title {
+  margin-bottom: 20px;
+}
+
 </style>
 
