2bac1f770e1ed08e94ebcd69a74fac5440baa4ef
いね <iineineno03k@gmail.com>
2024-12-14 19:52:29 +0900

    修正

--- backend/src/main/java/com/example/prcompass/controller/PrCompassController.java
+++ backend/src/main/java/com/example/prcompass/controller/PrCompassController.java
@@ -8,6 +8,7 @@ import com.example.prcompass.service.NotionService;
 import com.example.prcompass.service.ScrapingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class PrCompassController {
     @Autowired
     private JudgeService judgeService;
