11d744e8cb84062fa5427e36ba9f43dd2cf64b4c
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2024-12-14 18:36:16 +0900

    [ADD] Introduction component

---
+++ client/src/components/Introduction/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'Introduction',
+})
+
+</script>
+
+<template>
+  <img src="@/assets/PRCompassIntroduction.png"
+    alt="PRCompass Introduction"
+    width="450px"
+    height="150px"
+    class="PRCompass-introduction"
+  />
+</template>
+<style lang="css" scoped>
+.PRCompass-introduction {
+  display: block;
+}
+</style>
