036e6ca2c33594171c08299a27699f34ca18bb57
いね <iineineno03k@gmail.com>
2024-12-14 15:43:05 +0900

    backend initial commit

---
+++ backend/prcompass/src/main/java/com/example/prcompass/PrcompassApplication.java
@@ -0,0 +1,13 @@
+package com.example.prcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrcompassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrcompassApplication.class, args);
+    }
+
+}
