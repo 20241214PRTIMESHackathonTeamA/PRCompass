8c12e2d29335aae51555eb079bc975ed38710129
いね <iineineno03k@gmail.com>
2024-12-15 10:55:03 +0900

    キャッシュ破棄のクエリ追加

--- backend/src/main/java/com/example/prcompass/controller/PrCompassController.java
+++ backend/src/main/java/com/example/prcompass/controller/PrCompassController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,8 +29,8 @@ public class PrCompassController {
 
 
     @GetMapping("/title-recommend")
-    public ResponseEntity<RecommendResponse> getTitleRecommendations() {
-        var result = notionService.getRecommendations();
+    public ResponseEntity<RecommendResponse> getTitleRecommendations(@RequestParam(required = false) boolean refresh) {
+        var result = notionService.getRecommendations(refresh);
         return ResponseEntity.ok(result);
     }
 
--- backend/src/main/java/com/example/prcompass/service/NotionService.java
+++ backend/src/main/java/com/example/prcompass/service/NotionService.java
@@ -18,9 +18,13 @@ public class NotionService {
     private static final String CONTENT_KEY = "5Tpt1O8NX8ppm6djWu3ZwlpVAOVrYA1v2axR01CGFSaCTn2ohD0tw8jt7E6UQ1S6u0CdhJZbdjXXyIx3ct9iMVZP4dB2ZT23m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDwqcXZ0NqGxxgK4PTHqIy1_9K9ryjzYthiabeJtbY5-wmatAxFVTJf3UY4ERL4cgnoowShy1nYgJPk1HCswUcSM8cXT7sK5dtz9Jw9Md8uu&lib=MdJdi8-sFOQFJRM5UGI_Wm6Df4atSk3bP";
     private static final String NOTION_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=" + CONTENT_KEY;
 
-    private Cache<String, List<String>> cache = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.HOURS).build();
+    private Cache<String, List<String>> cache = CacheBuilder.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).build();
 
-    public RecommendResponse getRecommendations() {
+    public RecommendResponse getRecommendations(boolean refresh) {
+        if (refresh) {
+            cache.invalidate(CONTENT_KEY);
+        }
+
         List<String> newsTopics = cache.getIfPresent(CONTENT_KEY);
         if (newsTopics != null) {
             System.out.println("Cache hit");
